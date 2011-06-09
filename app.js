26ae64722a8552a95443cf1cff9fffa88f44464b
Aaron Bieber <qbit@deftly.net>
2011-06-09 07:56:17 -0700

    workign on generating the obj files

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var config = {
 	world: "/home/mine/minecraft_server/world",
-	mcobj: __dirname + '/mcobj',
+	mcobj: __dirname + '/bin/mcobj',
 	cpu: 2,
 	size: 20,
 	output_base: __dirname + '/tmp/',
 	debug: true
 };
 
+var exec = require( 'child_process' ).exec;
+var fs = require( 'fs' );
 var express = require( 'express' );
 var app = module.exports = express.createServer( );
+
+send404 = function(res){
+	res.writeHead(404);
+	res.write('404');
+	res.end();
+};
 
 app.configure( function( ){
 	app.set( 'views', __dirname + '/views' );
@@ -27,10 +35,22 @@ app.post( '/', function( req, res ) {
 	var x = req.body.loc.x / 16,
 		y = req.body.loc.y / 16,
 		z = req.body.loc.z / 16,
-		size = req.body.size || config.size;
+		s = req.body.size || config.size;
 	if ( config.debug ) console.log( "X: %d, Y: %d: Z: %d", x, y, z );
-
-
+	var cmd = config.mcobj + " -x="+x+" -y="+y+" -z="+z+" -s="+s+" -o="+config.output_base + "asdf.obj";
+	console.log( "'%s'", cmd );
+	var child = exec( cmd, function( error, stdout, stdin ) {
+		if ( error )  {
+			console.log( error );
+			return error;
+		} else {
+			fs.readFile( config.output_base + 'asdf.obj', function( err, data ) {
+				if ( err ) return send404( res );
+				res.writeHead( 200, { 'Content-Type': 'text/plain' } );
+				res.end( data, 'utf8' );
+			});
+		}
+	});
 });
 
 app.listen( 3000 );
